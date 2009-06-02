bd70149b318891e4d33fc4c54fd562ce72bbd733
Brandon Alexander <brandon_alexander@alexandermobile.com>
2009-06-02 00:03:46 -0500

    Initial commit. Basic Core Data project template.

---
+++ Classes/Find_an_ApartmentAppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  Find_an_ApartmentAppDelegate.h
+//  Find an Apartment
+//
+//  Created by Brandon Alexander on 6/2/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+@interface Find_an_ApartmentAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+- (IBAction)saveAction:sender;
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, readonly) NSString *applicationDocumentsDirectory;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
