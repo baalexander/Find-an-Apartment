6ff23beb1fdcde89e40483192ed373b5eabc57bc
Tyler Pearson <tyler@mushibo.com>
2009-07-01 14:54:48 -0500

    Added searching for cities and postal codes

--- Classes/PropertyCitiesViewController.m
+++ Classes/PropertyCitiesViewController.m
@@ -16,6 +16,9 @@
 @synthesize mainObjectContext = mainObjectContext_;
 @synthesize state = state_;
 @synthesize locationManager = locationManager_;
+@synthesize searchBar = searchBar_;
+@synthesize searchDisplayController = searchDisplayController_;
+@synthesize filteredContent = filteredContent_;
 
 
 #pragma mark -
@@ -36,6 +39,9 @@
     [fetchedResultsController_ release];
     [mainObjectContext_ release];
     [state_ release];
+    [searchBar_ release];
+    [searchDisplayController_ release];
+    [filteredContent_ release];
     
     [super dealloc];
 }
@@ -82,6 +88,20 @@
 {
     [super viewDidLoad];
     
+    // Search setup
+    self.filteredContent = [[NSArray alloc] init];
+    self.searchDisplayController = [[[UISearchDisplayController alloc]
+									 initWithSearchBar:self.searchBar contentsController:self] autorelease];
+    self.searchDisplayController.searchResultsDataSource = self;
+	self.searchDisplayController.searchResultsDelegate = self;
+    self.searchDisplayController.delegate = self;
+	
+	self.searchBar.autocorrectionType = UITextAutocorrectionTypeNo; // Don't get in the way of user typing.
+	self.searchBar.autocapitalizationType = UITextAutocapitalizationTypeNone; // Don't capitalize each word.
+	self.searchBar.scopeButtonTitles = [NSArray arrayWithObjects:@"All", @"Cities", @"Zips", nil];
+	self.searchBar.delegate = self; // Become delegate to detect changes in scope.
+    
+    
     // Setup the location button
     UIBarButtonItem *locationBtn = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"locate.png"]
                                                                     style:UIBarButtonItemStyleBordered 
@@ -119,17 +139,23 @@ static NSString *kSimpleCellId = @"SIMPLE_CELL_ID";
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
+    if(tableView == [[self searchDisplayController] searchResultsTableView])
+        return 1;
     return [[[self fetchedResultsController] sections] count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{    
+{
+    if(tableView == [[self searchDisplayController] searchResultsTableView])
+        return [self.filteredContent count];
     id <NSFetchedResultsSectionInfo> sectionInfo = [[[self fetchedResultsController] sections] objectAtIndex:section];
     return [sectionInfo numberOfObjects];
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section 
 {
+    if(tableView == [[self searchDisplayController] searchResultsTableView])
+        return @"";
     id<NSFetchedResultsSectionInfo> sectionInfo = [[[self fetchedResultsController] sections] objectAtIndex:section];
     return [[sectionInfo name] substringToIndex:(NSUInteger)1];
 }
@@ -143,7 +169,11 @@ static NSString *kSimpleCellId = @"SIMPLE_CELL_ID";
     }
     [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
     
-    CityOrPostalCode *city = [[self fetchedResultsController] objectAtIndexPath:indexPath];
+    CityOrPostalCode *city;
+    if(tableView == [[self searchDisplayController] searchResultsTableView])
+        city = [self.filteredContent objectAtIndex:indexPath.row];
+    else
+        city = [[self fetchedResultsController] objectAtIndexPath:indexPath];
     [[cell textLabel] setText:[[city value] description]];
     
     return cell;
@@ -155,7 +185,11 @@ static NSString *kSimpleCellId = @"SIMPLE_CELL_ID";
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    CityOrPostalCode *city = [[self fetchedResultsController] objectAtIndexPath:indexPath];
+    CityOrPostalCode *city;
+    if(tableView == [[self searchDisplayController] searchResultsTableView])
+        city = [self.filteredContent objectAtIndex:indexPath.row];
+    else
+        city = [[self fetchedResultsController] objectAtIndexPath:indexPath];
     
     PropertyCriteriaViewController *criteriaViewController = [[PropertyCriteriaViewController alloc] initWithNibName:@"PropertyCriteriaView" bundle:nil];
     [criteriaViewController setState:[self state]];
@@ -175,6 +209,69 @@ static NSString *kSimpleCellId = @"SIMPLE_CELL_ID";
     [criteriaViewController setCriteria:criteria];
     [[self navigationController] pushViewController:criteriaViewController animated:YES];
     [criteriaViewController release];
+}
+
+
+#pragma mark -
+#pragma mark Content Filtering
+
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+{	
+    NSLog(@"Search text: %@", searchText);
+    NSManagedObjectContext *geographyObjectContext = [[self state] managedObjectContext];
+	NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"CityOrPostalCode" 
+                                              inManagedObjectContext:geographyObjectContext];
+    [fetchRequest setEntity:entity];
+    
+    NSPredicate *predicate;// = [NSPredicate predicateWithFormat:@"state == %@ and value beginswith[cd] %@", [self state], searchText];
+    if([scope isEqualToString:@"All"])
+        predicate = [NSPredicate predicateWithFormat:@"state == %@ and value beginswith[cd] %@", [self state], searchText];
+    else if([scope isEqualToString:@"Cities"])
+        predicate = [NSPredicate predicateWithFormat:@"state == %@ and isCity == 1 and value beginsWith[cd] %@", [self state], searchText];
+    else
+        predicate = [NSPredicate predicateWithFormat:@"state == %@ and isCity == 0 and value beginsWith[cd] %@", [self state], searchText];
+    [fetchRequest setPredicate:predicate];
+    
+    NSSortDescriptor *cityDescriptor = [[NSSortDescriptor alloc] initWithKey:@"isCity" ascending:NO];
+    NSSortDescriptor *nameDescriptor = [[NSSortDescriptor alloc] initWithKey:@"value" ascending:YES];
+    NSArray *sortDescriptors = [[NSArray alloc] initWithObjects:cityDescriptor, nameDescriptor, nil];
+    [nameDescriptor release];
+    [cityDescriptor release];
+    [fetchRequest setSortDescriptors:sortDescriptors];
+    [sortDescriptors release];
+    
+    NSError *error = nil;
+    NSArray *res = [geographyObjectContext executeFetchRequest:fetchRequest error:&error];
+    self.filteredContent = res;
+    if(error)
+    {
+        NSLog(@"Error filtering content: %@", error);
+        // TODO: Handle search error
+    }
+}
+
+
+#pragma mark -
+#pragma mark UISearchDisplayController Delegate Methods
+
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString scope:
+     [[self.searchBar scopeButtonTitles] objectAtIndex:[self.searchBar selectedScopeButtonIndex]]];
+    
+    // Return YES to cause the search result table view to be reloaded.
+    return YES;
+}
+
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
+{
+    [self filterContentForSearchText:[self.searchBar text] scope:
+     [[self.searchBar scopeButtonTitles] objectAtIndex:searchOption]];
+    
+    // Return YES to cause the search result table view to be reloaded.
+    return YES;
 }
 
 @end
