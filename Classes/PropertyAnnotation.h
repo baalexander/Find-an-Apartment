7aff9d4052c773aaaa4142798ef25eb4175ef2e9
Tyler Pearson <tpearson@gmail.com>
2009-08-10 14:01:26 -0500

    Added summary to annotation so it can link to the details of the property

--- Classes/PropertyAnnotation.h
+++ Classes/PropertyAnnotation.h
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <MapKit/MapKit.h>
+@class PropertySummary;
 
 
 @interface PropertyAnnotation : NSObject <MKAnnotation>
@@ -7,10 +8,13 @@
     @private
         CLLocationCoordinate2D coordinate_;
         NSString *address_;
+        PropertySummary *summary_;
+        
 }
 
 @property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
 @property (nonatomic, retain) NSString *address;
+@property (nonatomic, retain) PropertySummary *summary;
 
 - (id)initWithCoordinate:(CLLocationCoordinate2D)coord;
 
--- Classes/PropertyAnnotation.m
+++ Classes/PropertyAnnotation.m
@@ -1,10 +1,13 @@
 #import "PropertyAnnotation.h"
 
+#import "PropertySummary.h"
+
 
 @implementation PropertyAnnotation
 
 @synthesize coordinate = coordinate_;
 @synthesize address = address_;
+@synthesize summary = summary_;
 
 
 - (id)initWithCoordinate:(CLLocationCoordinate2D)coord
@@ -19,6 +22,14 @@
 - (NSString *)title
 {
     return [self address];
+}
+
+- (void)dealloc
+{
+    [address_ release];
+    [summary_ release];
+    
+    [super dealloc];
 }
 
 @end
