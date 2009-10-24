3f479f40cb8c25ef11371f65d07bdd0c2f24569b
Brandon Alexander <baalexander@gmail.com>
2009-10-23 23:59:49 -0500

    Added comment about git command to ignore changes to the Google Maps key file.

--- Classes/GeocoderGoogleMapsKey.h
+++ Classes/GeocoderGoogleMapsKey.h
@@ -1,3 +1,5 @@
 // Replace with your Google Maps API key
 // Can sign up here: http://code.google.com/apis/maps/signup.html
+// Use this git command to ignore your change in git:
+// git update-index --assume-unchanged Classes/GeocoderGoogleMapsKey.h
 #define kGoogleMapsKey @"REPLACE_WITH_YOUR_KEY"
