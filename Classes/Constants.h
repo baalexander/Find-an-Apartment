c83a4be8b1bfce6acfb750b184a9c631a816803b
Brandon Alexander <baalexander@gmail.com>
2010-02-10 23:41:00 -0600

    Changed version to 3.0 for both apps. Cleaned up info plist files.

--- Classes/Constants.h
+++ Classes/Constants.h
@@ -1,7 +1,7 @@
 #ifdef HOME_FINDER
 #define kAppName @"Home Finder"
-#define kAppVersion @"2.0"
+#define kAppVersion @"3.0"
 #else
 #define kAppName @"Find an Apartment"
-#define kAppVersion @"2.0.1"
+#define kAppVersion @"3.0"
 #endif
